8c743bb01022eff7fb2562369c480dbacbe4b47f
Daniel Nguyen <danielhuannguyen@gmail.com>
2015-08-26 15:41:03 -0500

    Adding some pointer stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
 int main () {
-	std::cout<<"Hello world"<<std::endl;
-	std::cin.get();
+	int size;
+	std::string* array;
+
+
+	std::cout << "Pointers example" << std::endl;
+	std::cout << "Enter a number" << std::endl;
+	std::cin >> size;
+
+	std::cout << "size : " << size << std::endl;
+
+	array = new std::string[size];
+
+	for (int i = 0; i < size; i++) {
+		std::cout << "String " << i << ": ";
+		std::cin >> array[i];
+	}
+
+	for (int i = 0; i < size; i++) {
+		std::cout << "array[" << i << "]: " << array[i] << std::endl;
+	}
+
+	delete[] array;
 	return 0;
 }
